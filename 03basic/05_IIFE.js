e1e652d2c40bfbc2416c39de57a47e828690e4ca
abhisheksaxena15 <saxenaabhishek9009@gmail.com>
2024-09-22 00:46:13 +0530

    IIFE

--- 03basic/05_IIFE.js
+++ 03basic/05_IIFE.js
@@ -6,15 +6,22 @@
     console.log("hello welcome to my channel");
 })();
 
+
+
 (() => {
     console.log("hello son i am your dad");
 })();
+
+
 
 (function(name){               //-----> Parameterized iife
     console.log(`${name} hello welcome to my channel`);
 })(
     "abhishek"
 );
+
+
+
 
 ((name) => {                    //-----> Parameterized iife
     console.log(`${name} hello welcome to my website`)
